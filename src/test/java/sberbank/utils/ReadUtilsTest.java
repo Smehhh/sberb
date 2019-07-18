73e6828f3625bd3bb3c29aa8f9ac24061802e8db
eqein <e.qein666@gmail.com>
2019-07-18 16:50:53 +0300

    add some tests

--- src/test/java/sberbank/utils/ReadUtilsTest.java
+++ src/test/java/sberbank/utils/ReadUtilsTest.java
@@ -1,14 +1,29 @@
 package sberbank.utils;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReadUtilsTest {
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
     @Test
     public void filesReader() {
         ReadUtils readUtils = new ReadUtils();
         String act = readUtils.filesReader(42);
         Assert.assertEquals("Hello", act);
     }
+    @Test
+    public void returnExe() {
+        ReadUtils readUtils = new ReadUtils();
+        Assert.assertEquals("", readUtils.filesReader(21));
+    }
+
 }
