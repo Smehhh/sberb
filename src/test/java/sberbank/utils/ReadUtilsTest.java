b6f2836841ff7b6c138934563d076e27942bdf5d
Smehhh <51404705+Smehhh@users.noreply.github.com>
2019-07-18 17:07:39 +0300

    Update ReadUtilsTest

--- src/test/java/sberbank/utils/ReadUtilsTest.java
+++ src/test/java/sberbank/utils/ReadUtilsTest.java
@@ -11,9 +11,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ReadUtilsTest {
-
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
     @Test
     public void filesReader() {
         ReadUtils readUtils = new ReadUtils();
